2aa3ac73c3793ed90493d3437a6363327665bbf5
= <=>
2024-02-22 19:37:33 +0600

    KS-1-1 transferred data from Main to lesson1_task1

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -1,5 +1,6 @@
-package org.example.lesson_1
-
-fun main(){
-
+fun main() {
+    // var - потому что количество заказов должно меняться.
+    // UInt потому что количество заказов не может быть отрицательным.
+    var totalNumberOfOrders: UInt = 75u
+    val thankYouText: String = "Благодарим за покупку в нашем магазине!"
 }
