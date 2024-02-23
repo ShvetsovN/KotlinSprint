e10d525320125eaa73d936c39363b84d9c007907
= <=>
2024-02-23 15:54:05 +0600

    KS-1-5 add const variable, timeInSpace variable update calculation

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,8 +1,14 @@
 package lesson_1
 
-fun main(){
-    val hours = 6480/60/60
-    val minutes = 6480/60%60
-    val seconds = 6480%60
+const val SECONDS_IN_MINUTE = 60
+
+fun main() {
+
+    val timeInSpace = 6480
+
+    val hours = timeInSpace / SECONDS_IN_MINUTE / SECONDS_IN_MINUTE
+    val minutes = timeInSpace / SECONDS_IN_MINUTE % SECONDS_IN_MINUTE
+    val seconds = timeInSpace % SECONDS_IN_MINUTE
     print("%02d".format(hours) + ':' + "%02d".format(minutes) + ':' + "%02d".format(seconds))
 }
+
