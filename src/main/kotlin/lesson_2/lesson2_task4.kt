522f2d1b6d5f47b4e52e12758b87b4dbd59f7d61
= <=>
2024-02-26 14:45:47 +0600

    KS-2-4 upadted variables buff

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,13 +1,12 @@
 package org.example.lesson_1.lesson_2
 
-const val BUFF = 0.20f
-
 fun main(){
 
+    val buffInPercent= 20
     val cristalOre = 7
     val ironOre = 11
 
-    println("Количество бонусных материалов кристалической руды - ${(cristalOre * BUFF).toInt()}")
-    println("Количество бонусных материалов железной руды - ${(ironOre * BUFF).toInt()}")
+    println("Количество бонусных материалов кристалической руды - ${(cristalOre * (buffInPercent/100f)).toInt()}")
+    println("Количество бонусных материалов железной руды - ${(ironOre * (buffInPercent/100f)).toInt()}")
 
 }
