16eec236fd23aa66a9e179c5f1186781655ca285
= <=>
2024-02-27 21:18:47 +0600

    KS-5-1 update solution

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -3,6 +3,6 @@ package org.example.lesson_1.lesson_5
 fun main() {
 
     println("Здравствуйте! Введите 2 числа так, чтобы их сумма была больше 10: ")
-    println(if ((readln().toInt() + readln().toInt()) >= 10) "Добро пожаловать!" else "Доступ закрыт!")
+    println(if ((readln().toInt() + readln().toInt()) > 10) "Добро пожаловать!" else "Доступ закрыт!")
 
 }
