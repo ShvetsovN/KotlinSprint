64eb3513491f7d718b1215c5f2b86a766a50b712
= <=>
2024-03-01 16:50:48 +0600

    KS-5-5 add solution

--- src/main/kotlin/lesson_5/lesson5_task5.kt
+++ src/main/kotlin/lesson_5/lesson5_task5.kt
@@ -1,46 +1,29 @@
 package org.example.lesson_1.lesson_5
 
-const val WINNING_NUMBER_ONE = 13
-const val WINNING_NUMBER_TWO = 6
-const val WINNING_NUMBER_THREE = 17
+import kotlin.random.Random
 
 const val NUMBER_MIN = 0
 const val NUMBER_MAX = 42
+const val AMOUNT_OF_NUMBER = 3
 
 fun main() {
 
-    println("Добро пожаловать в лотерею удачи! Необходимо ввести 2 числа от 0 до 42. Введите первое число: ")
-    var numberOne = readln().toInt()
-    if (numberOne < NUMBER_MIN) {
-        println("Ай ай, просили же от 0 до 42, теперь первое число будет 0")
-        numberOne = NUMBER_MIN
-    } else if (numberOne > NUMBER_MAX) {
-        println("Ай ай, просили же от 0 до 42, теперь первое число будет 42")
-        numberOne = NUMBER_MAX
-    }
-    println("Вы ввели число: $numberOne")
+    val winningNumber = List(3) {Random.nextInt(NUMBER_MIN, NUMBER_MAX)}.toMutableList()
+    val userNumber = mutableListOf<Int>()
+    val winningNumberText = "Загаданные числа это - ${winningNumber[0]}, ${winningNumber[1]}, ${winningNumber[2]}"
 
-    println("Введите второе число: ")
-    var numberTwo = readln().toInt()
-    if (numberTwo < NUMBER_MIN) {
-        println("Ай ай, просили же от 0 до 42, теперь второе число будет 0")
-        numberTwo = NUMBER_MIN
-    } else if (numberTwo > NUMBER_MAX) {
-        println("Ай ай, просили же от 0 до 42, теперь втрое число будет 42")
-        numberTwo = NUMBER_MAX
-    }
-    println("Вы ввели число: $numberTwo")
-
-    val result = when {
-        (numberOne == WINNING_NUMBER_ONE && numberTwo == WINNING_NUMBER_TWO)
-                || (numberOne == WINNING_NUMBER_TWO && numberTwo == WINNING_NUMBER_ONE) -> "Вы выиграли главный приз!"
-
-        (numberOne == WINNING_NUMBER_ONE || numberTwo == WINNING_NUMBER_TWO)
-                || (numberOne == WINNING_NUMBER_TWO || numberTwo == WINNING_NUMBER_ONE) -> "Вы выиграли утешительный приз! \nВыиграшные числа это $WINNING_NUMBER_ONE и $WINNING_NUMBER_TWO"
-
-        else -> "Неудача! \nВыиграшные числа это $WINNING_NUMBER_ONE и $WINNING_NUMBER_TWO"
+    for(i in 1 .. 3) {
+        println("Добро пожаловать в лотерею удачи! Необходимо ввести $AMOUNT_OF_NUMBER числа от $NUMBER_MIN до $NUMBER_MAX. Введите $i число: ")
+        userNumber.add(readln().toInt())
     }
 
-    println(result)
+    val result = userNumber.intersect(winningNumber.toSet()).toList()
+
+    when(result.size){
+        3 -> println("Вы угадали три числа! Вы выиграли Джекпот! $winningNumberText")
+        2 -> println("Вы угадали два числа! Вы выиграли крупный приз! $winningNumberText")
+        1 -> println("Вы угадали одно три числа! Вы выиграли утишительный приз! $winningNumberText")
+        else -> println("Вы не угадали ни одного числа! $winningNumberText")
+    }
 
 }
