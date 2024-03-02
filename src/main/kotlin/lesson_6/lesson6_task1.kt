1a5fad5b34e4a17979164525194ed1934616d9bd
= <=>
2024-03-02 14:24:26 +0500

    KS-6-1 add solution

--- src/main/kotlin/lesson_6/lesson6_task1.kt
+++ src/main/kotlin/lesson_6/lesson6_task1.kt
@@ -1,14 +1,31 @@
 package org.example.lesson_1.lesson_6
 
-fun main(){
+fun main() {
+
+    var isAuthorization = true
 
     println("Задайте логин: ")
-    val userLogin = readln()
+    val userName = readln()
 
-    println("Задайте пароль: ")
+    println("Отлично, теперь пароль: ")
     val userPassword = readln()
 
-    do{
+    println("Регистрация пройдена, введите еще раз логин и пароль для подтверждения: ")
 
-    } while()
+    while (isAuthorization) {
+        println("Введите логин для авторизации: ")
+        val userRegistrationName = readln()
+        println("Введите пароль для авторизации: ")
+        val userRegistrationPassword = readln()
+        when {
+            userRegistrationPassword == userPassword &&
+                    userRegistrationName == userName -> isAuthorization = false
+
+            else -> {
+                println("Вы ввели не верные данные")
+                isAuthorization = true
+            }
+        }
+    }
+    println("Авторизация прошла успешно")
 }
