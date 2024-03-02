c44b04890c32a03224e3c68c13541423254f8277
= <=>
2024-03-02 13:36:26 +0500

    KS-6-1 add solution

---
+++ src/main/kotlin/lesson_6/lesson6_task1.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_1.lesson_6
+
+fun main(){
+
+    println("Задайте логин: ")
+    val userLogin = readln()
+
+    println("Задайте пароль: ")
+    val userPassword = readln()
+
+    do{
+
+    } while()
+}
