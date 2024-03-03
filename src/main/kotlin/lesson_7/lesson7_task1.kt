21937a3666561059cadfc559f4bfe676c75a3f1b
= <=>
2024-03-03 18:56:10 +0500

    KS-7-1 update solution add 2 variables charArray and numberArray, removed magic numbers

--- src/main/kotlin/lesson_7/lesson7_task1.kt
+++ src/main/kotlin/lesson_7/lesson7_task1.kt
@@ -1,13 +1,13 @@
 package org.example.lesson_1.lesson_7
 
-import kotlin.random.Random
-
 fun main() {
 
     var generatedPassword = ""
+    val charArray = 'a'..'z'
+    val numberArray = 0..9
 
-    for (i in 0..2) generatedPassword += "${Random.nextInt(0, 9)}" +
-            "${Random.nextInt(97, 122).toChar()}"
+    for (i in 0..2) generatedPassword += "${numberArray.random()}" +
+            "${charArray.random()}"
 
     println(generatedPassword)
 
