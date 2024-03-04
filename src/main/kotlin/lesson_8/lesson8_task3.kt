266e1ca17a6687a4fdf7c2d832b32cc4f3e4d339
= <=>
2024-03-04 15:12:44 +0500

    KS-8-3 solution added

---
+++ src/main/kotlin/lesson_8/lesson8_task3.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_1.lesson_8
+
+fun main() {
+
+    val arrayOfIngredientsForBagel = arrayOf("flour", "sugar", "water", "oil", "salt")
+
+    val lookingForIngredients = readln()
+
+    if(lookingForIngredients in arrayOfIngredientsForBagel) println("Ингридиент $lookingForIngredients в рецепте есть")
+    else println("Такого ингредиента в рецепте нет")
+
+}
