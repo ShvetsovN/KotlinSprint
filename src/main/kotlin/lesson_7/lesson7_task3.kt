158cb4826f9dd93b7b62688365fde2881007e71d
= <=>
2024-03-04 14:03:47 +0500

    KS-7-3 updated solution added space

--- src/main/kotlin/lesson_7/lesson7_task3.kt
+++ src/main/kotlin/lesson_7/lesson7_task3.kt
@@ -1,12 +1,13 @@
 package org.example.lesson_1.lesson_7
 
-fun main(){
+fun main() {
 
     println("Добро пожаловать! Введите, пожалуйста, число: ")
+
     val userInput = readln().toInt()
 
-    for(i in 0..userInput){
-        if(i%2 == 0) print("$i ")
+    for (i in 0..userInput) {
+        if (i % 2 == 0) print(i)
     }
 
 }
