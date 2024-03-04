41f6826dc2d959d41f9ffb376c945ec9b5085e9e
= <=>
2024-03-04 16:36:49 +0500

    KS-8-4 solution added

---
+++ src/main/kotlin/lesson_8/lesson8_task4.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_1.lesson_8
+
+fun main() {
+
+    val arrayOfIngredients = arrayOf("сахар", "вода", "соль", "перец", "мука", "яйцо")
+    println(arrayOfIngredients.contentToString())
+
+    println("Какой ингредиент мы хотим заменить?")
+    val userInputNewIngredients = readln()
+
+    for (i in arrayOfIngredients.indices) {
+        if (arrayOfIngredients[i] == userInputNewIngredients) {
+            println("Какой ингредиент добавить? ")
+            arrayOfIngredients[i] = readln()
+            println("Готово! Вы сохранили следующий список: ${arrayOfIngredients.contentToString()}")
+            return
+        }
+    }
+    println("Такого ингредиента нет в рецепте")
+}
