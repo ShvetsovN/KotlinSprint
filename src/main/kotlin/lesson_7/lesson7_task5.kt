9b708ee714066b44ebc746960356da7526f0cd6f
= <=>
2024-03-04 21:24:27 +0500

    KS-7-5 solution updated tried new variables tried new loop

--- src/main/kotlin/lesson_7/lesson7_task5.kt
+++ src/main/kotlin/lesson_7/lesson7_task5.kt
@@ -3,7 +3,10 @@ package org.example.lesson_1.lesson_7
 fun main() {
 
     var numberOfPasswordSymbols: Int
-    val charArray = 33..134
+    val requiredCharacters = ('a'..'z') + ('A'..'Z') +
+                (0..9)
+    val optionalCharacters = ('!'..'/') + (':'..'@') + ('['..'`')
+    var generatedNewPassword = ""
 
     println("Добро пожаловать! Введите какой длинны пароль вы бы хотели (не меньше 6): ")
 
@@ -11,9 +14,10 @@ fun main() {
         numberOfPasswordSymbols = readln().toInt()
     } while (numberOfPasswordSymbols < 6)
 
-    var generatedNewPassword = ""
-
-    for (i in 0..numberOfPasswordSymbols) generatedNewPassword += "${charArray.random().toChar()}"
+    for (i in 0 until numberOfPasswordSymbols) {
+        if (i in 0..5) generatedNewPassword += requiredCharacters.random()
+        if (i in 6..numberOfPasswordSymbols) generatedNewPassword += optionalCharacters.random()
+    }
 
     println("Вот ваш новый пароль: $generatedNewPassword")
 
