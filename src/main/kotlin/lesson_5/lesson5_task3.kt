d34aa8cac7dc1f3678c72fb7b8ca1d1d5c4d1386
= <=>
2024-03-05 18:12:08 +0500

    updated const variables names

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -1,44 +1,44 @@
 package org.example.lesson_1.lesson_5
 
-const val WINNING_NUMBER_ONE = 13
-const val WINNING_NUMBER_TWO = 6
+const val WINNING_NUMBER_ONE_53 = 13
+const val WINNING_NUMBER_TWO_53 = 6
 
-const val NUMBER_MIN = 0
-const val NUMBER_MAX = 42
+const val NUMBER_MIN_53 = 0
+const val NUMBER_MAX_53 = 42
 
 fun main() {
 
     println("Добро пожаловать в лотерею удачи! Необходимо ввести 2 числа от 0 до 42. Введите первое число: ")
     var numberOne = readln().toInt()
-    if (numberOne < NUMBER_MIN) {
+    if (numberOne < NUMBER_MIN_53) {
         println("Ай ай, просили же от 0 до 42, теперь первое число будет 0")
-        numberOne = NUMBER_MIN
-    } else if (numberOne > NUMBER_MAX) {
+        numberOne = NUMBER_MIN_53
+    } else if (numberOne > NUMBER_MAX_53) {
         println("Ай ай, просили же от 0 до 42, теперь первое число будет 42")
-        numberOne = NUMBER_MAX
+        numberOne = NUMBER_MAX_53
     }
     println("Вы ввели число: $numberOne")
 
     println("Введите второе число: ")
     var numberTwo = readln().toInt()
-    if (numberTwo < NUMBER_MIN) {
+    if (numberTwo < NUMBER_MIN_53) {
         println("Ай ай, просили же от 0 до 42, теперь второе число будет 0")
-        numberTwo = NUMBER_MIN
-    } else if (numberTwo > NUMBER_MAX) {
+        numberTwo = NUMBER_MIN_53
+    } else if (numberTwo > NUMBER_MAX_53) {
         println("Ай ай, просили же от 0 до 42, теперь втрое число будет 42")
-        numberTwo = NUMBER_MAX
+        numberTwo = NUMBER_MAX_53
     }
     println("Вы ввели число: $numberTwo")
 
     val result = when {
-        numberOne == WINNING_NUMBER_ONE && numberTwo == WINNING_NUMBER_TWO ||
-                numberOne == WINNING_NUMBER_TWO && numberTwo == WINNING_NUMBER_ONE -> "Вы выиграли главный приз!"
+        numberOne == WINNING_NUMBER_ONE_53 && numberTwo == WINNING_NUMBER_TWO_53 ||
+                numberOne == WINNING_NUMBER_TWO_53 && numberTwo == WINNING_NUMBER_ONE_53 -> "Вы выиграли главный приз!"
 
-        numberOne == WINNING_NUMBER_ONE || numberTwo == WINNING_NUMBER_TWO ||
-                numberOne == WINNING_NUMBER_TWO || numberTwo == WINNING_NUMBER_ONE -> "Вы выиграли утешительный приз! \n" +
-                "Выиграшные числа это $WINNING_NUMBER_ONE и $WINNING_NUMBER_TWO"
+        numberOne == WINNING_NUMBER_ONE_53 || numberTwo == WINNING_NUMBER_TWO_53 ||
+                numberOne == WINNING_NUMBER_TWO_53 || numberTwo == WINNING_NUMBER_ONE_53 -> "Вы выиграли утешительный приз! \n" +
+                "Выиграшные числа это $WINNING_NUMBER_ONE_53 и $WINNING_NUMBER_TWO_53"
 
-        else -> "Неудача! \nВыиграшные числа это $WINNING_NUMBER_ONE и $WINNING_NUMBER_TWO"
+        else -> "Неудача! \nВыиграшные числа это $WINNING_NUMBER_ONE_53 и $WINNING_NUMBER_TWO_53"
     }
 
     println(result)
--- src/main/kotlin/lesson_5/lesson5_task5.kt
+++ src/main/kotlin/lesson_5/lesson5_task5.kt
@@ -2,18 +2,18 @@ package org.example.lesson_1.lesson_5
 
 import kotlin.random.Random
 
-const val NUMBER_MIN = 0
-const val NUMBER_MAX = 42
-const val AMOUNT_OF_NUMBER = 3
+const val NUMBER_MIN_55 = 0
+const val NUMBER_MAX_55 = 42
+const val AMOUNT_OF_NUMBER_55 = 3
 
 fun main() {
 
-    val winningNumber = List(3) {Random.nextInt(NUMBER_MIN, NUMBER_MAX)}.toMutableList()
+    val winningNumber = List(3) {Random.nextInt(NUMBER_MIN_55, NUMBER_MAX_55)}.toMutableList()
     val userNumber = mutableListOf<Int>()
     val winningNumberText = "Загаданные числа это - ${winningNumber[0]}, ${winningNumber[1]}, ${winningNumber[2]}"
 
     for(i in 1 .. 3) {
-        println("Добро пожаловать в лотерею удачи! Необходимо ввести $AMOUNT_OF_NUMBER числа от $NUMBER_MIN до $NUMBER_MAX. Введите $i число: ")
+        println("Добро пожаловать в лотерею удачи! Необходимо ввести $AMOUNT_OF_NUMBER_55 числа от $NUMBER_MIN_55 до $NUMBER_MAX_55. Введите $i число: ")
         userNumber.add(readln().toInt())
     }
 
