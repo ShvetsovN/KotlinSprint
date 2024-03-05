6f71e06e4e6b86a4cbd0de81656323a077449540
= <=>
2024-03-05 13:56:36 +0500

    KS-8-3 solution updated question for the user added

--- src/main/kotlin/lesson_8/lesson8_task3.kt
+++ src/main/kotlin/lesson_8/lesson8_task3.kt
@@ -2,8 +2,9 @@ package org.example.lesson_1.lesson_8
 
 fun main() {
 
-    val arrayOfIngredientsForBagel = arrayOf("flour", "sugar", "water", "oil", "salt")
+    println("Какой ингредиент мы ищем?")
 
+    val arrayOfIngredientsForBagel = arrayOf("flour", "sugar", "water", "oil", "salt")
     val lookingForIngredients = readln()
 
     if(lookingForIngredients in arrayOfIngredientsForBagel) println("Ингридиент $lookingForIngredients в рецепте есть")
