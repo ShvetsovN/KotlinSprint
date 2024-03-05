f7a1c62b998d404089bbd19a4a8e7f6a42b9391f
= <=>
2024-03-05 17:59:48 +0500

    KS-7-5 solution updated added shuffle function removed special symbols added 3 variables for 2 charRange and 1 intRange

--- src/main/kotlin/lesson_7/lesson7_task5.kt
+++ src/main/kotlin/lesson_7/lesson7_task5.kt
@@ -3,9 +3,11 @@ package org.example.lesson_1.lesson_7
 fun main() {
 
     var numberOfPasswordSymbols: Int
+    val lowercaseLetterForPass = 'a'..'z'
+    val uppercaseLetterForPass = 'A'..'Z'
+    val numbersForPass = 0..9
     val requiredCharacters = ('a'..'z') + ('A'..'Z') +
-                (0..9)
-    val optionalCharacters = ('!'..'/') + (':'..'@') + ('['..'`')
+            (0..9)
     var generatedNewPassword = ""
 
     println("Добро пожаловать! Введите какой длинны пароль вы бы хотели (не меньше 6): ")
@@ -14,11 +16,15 @@ fun main() {
         numberOfPasswordSymbols = readln().toInt()
     } while (numberOfPasswordSymbols < 6)
 
-    for (i in 0 until numberOfPasswordSymbols) {
-        if (i in 0..5) generatedNewPassword += requiredCharacters.random()
-        if (i in 6..numberOfPasswordSymbols) generatedNewPassword += optionalCharacters.random()
-    }
+    generatedNewPassword += lowercaseLetterForPass.random()
+    generatedNewPassword += uppercaseLetterForPass.random()
+    generatedNewPassword += numbersForPass.random()
 
-    println("Вот ваш новый пароль: $generatedNewPassword")
+    for (i in 4..numberOfPasswordSymbols) {
+        generatedNewPassword += requiredCharacters.random()
+    }
+    val nextGeneratedPassword = generatedNewPassword.toList().shuffled()
+
+    println("Вот ваш новый пароль: ${nextGeneratedPassword.joinToString("")}")
 
 }
