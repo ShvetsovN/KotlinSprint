de6b91b9db2083f97d915d9154a92a0fb1905500
= <=>
2024-03-06 17:33:27 +0500

    KS-9-5 solution updated replaced list to HashSet added .lowercase for all output except the first word

--- src/main/kotlin/lesson_9/lesson9_task5.kt
+++ src/main/kotlin/lesson_9/lesson9_task5.kt
@@ -2,17 +2,17 @@ package org.example.lesson_1.lesson_9
 
 fun main() {
 
-    val listOfIngredients = mutableListOf<String>()
+    val setOfIngredients = HashSet<String>()
 
     println("Введите 5 ингредиентов (каждый по отдельности)")
 
     repeat(5) {
-        listOfIngredients.add(readln())
+        setOfIngredients.add(readln())
     }
 
-    val listOfIngredientsSorted = listOfIngredients.distinct().sorted()
-    listOfIngredientsSorted.forEach { it ->
-        print("${it.replaceFirstChar { it.uppercase() }}, ")
+    val setOfIngredientsSorted = setOfIngredients.sorted()
+    setOfIngredientsSorted.forEach { it ->
+        if (it == setOfIngredientsSorted[0]) print("${it.lowercase().replaceFirstChar { it.uppercaseChar() }}, ")
+        else print("${it.lowercase()}, ")
     }
-
 }
