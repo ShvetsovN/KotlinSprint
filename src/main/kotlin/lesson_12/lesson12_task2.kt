724fcac482c89350842ab7210cc9c4db13cfdb3a
= <=>
2024-03-09 20:03:30 +0500

    KS-12-2 solution updated renamed objects (weatherTracker1,2 to weatherTracker3,4)to avoid conflict

--- src/main/kotlin/lesson_12/lesson12_task2.kt
+++ src/main/kotlin/lesson_12/lesson12_task2.kt
@@ -1,11 +1,11 @@
 package org.example.lesson_1.lesson_12
 
 fun main() {
-    val weatherTracker1 = WeatherTracker2(7, 8, true)
-    weatherTracker1.weatherOutput()
+    val weatherTracker3 = WeatherTracker2(7, 8, true)
+    weatherTracker3.weatherOutput()
 
-    val weatherTracker2 = WeatherTracker2(1, 0, false)
-    weatherTracker2.weatherOutput()
+    val weatherTracker4 = WeatherTracker2(1, 0, false)
+    weatherTracker4.weatherOutput()
 }
 
 class WeatherTracker2(val daytimeTemperature: Int, val nightTemperature: Int, val isPrecipitation: Boolean,) {
