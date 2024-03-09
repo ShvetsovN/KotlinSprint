d9d27f6ba3c257b10759eb4a83899b74da7fea2d
= <=>
2024-03-09 17:01:22 +0500

    KS-11-3 solution updated re-read and corrected for TS

--- src/main/kotlin/lesson_11/lesson11_task3.kt
+++ src/main/kotlin/lesson_11/lesson11_task3.kt
@@ -3,7 +3,6 @@ package org.example.lesson_1.lesson_11
 fun main() {
 
     val essense1 = Essense(
-        id = 1,
         nickname = "Мистер Пиклз",
         avatar = "Пикуль"
     )
@@ -13,28 +12,35 @@ fun main() {
         name = "Комната общения",
     )
 
-    println("Добро пожаловать ${essense1.nickname}! Желаете посетить комнату?")
-    if (readln().equals("да", true)) {
-        someRoom1.userAdd(someRoom1, essense1)
-    }
-
-    println("Желаете обновить ваш статус ${essense1.nickname}?")
-    if (readln().equals("да", true)) essense1.updateStatus()
-
-    println("Хотите узнать информацию о юзере? Нажмите на аватар на 2 секунды")
-    if (readln().toInt() == 2) someRoom1.infoUsers(essense1)
+    someRoom1.userAdd(someRoom1, essense1)
+    someRoom1.updateStatus(essense1)
+    someRoom1.infoUsers(essense1)
 
 }
 
 class Essense(
-    val id: Int,
     val nickname: String,
     var status: String = "пользователь заглушен",
     val avatar: String,
 ) {
+}
 
-    fun updateStatus() {
-        println("Ваш статус: $status")
+class SomeRoom(
+    val cover: String,
+    val name: String,
+    val listOfUsers: HashSet<String> = hashSetOf(),
+) {
+
+    fun userAdd(room: SomeRoom, user: Essense) {
+        println("Добро пожаловать ${user.nickname}! ")
+        listOfUsers.add(user.nickname)
+        println(
+            "В комнате \"${room.cover}\", c названием \"${room.name}\" теперь ${room.listOfUsers}!"
+        )
+    }
+
+    fun updateStatus(user: Essense) {
+        println("Ваш статус: ${user.status}")
         println(
             """Выберите желаемый статус: 
             |1 - разговаривает
@@ -44,43 +50,23 @@ class Essense(
         )
         when (readln().toInt()) {
             1 -> {
-                status = "разговаривает"
-                println("Ваш статус обновлен на: $status")
+                user.status = "разговаривает"
+                println("Ваш статус обновлен на: ${user.status}")
             }
 
             2 -> {
-                status = "микрофон выключен"
-                println("Ваш статус обновлен на: $status")
+                user.status = "микрофон выключен"
+                println("Ваш статус обновлен на: ${user.status}")
             }
 
             else -> {
-                println("Пользователь не стал менять статус: $status")
+                println("Пользователь не стал менять статус: ${user.status}")
             }
         }
-
-    }
-
-}
-
-class SomeRoom(
-    val cover: String,
-    val name: String,
-    val listOfUsers: HashSet<String> = hashSetOf(),
-    var statusRoom: String = "пуста",
-) {
-
-    fun userAdd(room: SomeRoom, user: Essense) {
-        println("Добро пожаловать ${user.nickname}! ")
-        listOfUsers.add(user.nickname)
-        statusRoom = "Есть посетители"
-        println(
-            "В комнате \"${room.cover}\", c названием \"${room.name}\" теперь ${room.listOfUsers}! Статус комнаты изменен" +
-                    "на \"$statusRoom\""
-        )
     }
 
     fun infoUsers(user: Essense) {
-        println("Если долго нажимать на аватарку то...")
-        println("ID: ${user.id}, имя пользователя: ${user.nickname}, статус: ${user.status}, аватар: ${user.avatar}")
+        println("После 2х секундного нажатия на аватарку выводиться сообщение: \n" +
+                "Имя пользователя: ${user.nickname}, статус: ${user.status}, аватар: ${user.avatar}")
     }
 }
