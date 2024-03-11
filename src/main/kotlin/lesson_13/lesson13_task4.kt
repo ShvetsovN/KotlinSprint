25501a463f6c9185444e3b18c6475ba72fa81fdf
= <=>
2024-03-11 19:11:46 +0500

    KS-13-4 solution updated

--- src/main/kotlin/lesson_13/lesson13_task4.kt
+++ src/main/kotlin/lesson_13/lesson13_task4.kt
@@ -1,32 +1,37 @@
 package org.example.lesson_1.lesson_13
 
 fun main() {
-
-    var newContacts4: Contacts4
     val contactList = mutableListOf<Contacts4>()
 
     do {
-        println("Укажите имя, номер телефона и название компании (каждое с новой строки)")
-        newContacts4 = Contacts4(readln(), readln().toLongOrNull(), readln())
-        if (newContacts4.phoneNumber == null) {
-            println("Вы не указали номер телефона")
-            continue
-        }
-        contactList.add(newContacts4)
-    } while (newContacts4.name != null && newContacts4.phoneNumber != null && newContacts4.companyName != null)
+        println("Укажите имя: ")
+        val name = readln()
+
+        println("Укажите номер телефона: ")
+        val phoneNumber = readLine()?.toLongOrNull()
+
+        println("Укажите название компании: ")
+        val companyName = readLine()
+
+        if (phoneNumber != null) contactList.add(Contacts4(name, phoneNumber, companyName))
+        else println("Вы не указали номер телефона")
+
+        println("Добавить еще один контакт? да/нет: ")
+
+    } while (readln().equals("да", true))
 
     contactList.forEach { it.printInformation() }
 
 }
 
 class Contacts4(
-    val name: String?,
+    val name: String,
     val phoneNumber: Long?,
     val companyName: String?,
 ) {
 
     fun printInformation() {
         println("***\nИмя: $name\nНомер: $phoneNumber\nКомпания: ${companyName ?: "<не указано>"}")
-    }
 
+    }
 }
