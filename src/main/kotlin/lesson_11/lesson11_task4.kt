4d571b125c0f234eb301d2624283602c3aca2bfd
= <=>
2024-03-12 16:34:00 +0500

    KS-11-4 solution updated renamed class RecipeCategories to RecipeCategory renamed class Ingredients to Ingredient added variables: imageUrl (RecipeCategory, Recipe), cookingMethod (Recipe) changed all Set to List

--- src/main/kotlin/lesson_11/lesson11_task4.kt
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -1,48 +1,22 @@
 package org.example.lesson_1.lesson_11
 
-fun main() {
-
-    val tvorog = Ingredients(
-        1,
-        "творог",
-    )
-
-    val aqua = Ingredients(
-        2,
-        "вода"
-    )
-
-    val burger = Recipe(
-        1,
-        "Чизбургер",
-        setOf(tvorog, aqua)
-    )
-
-    val burgers = RecipeCategories(
-        1,
-        "Бургеры",
-        setOf(burger )
-    )
-
-    println("ID: ${burgers.id}, NameOfCategories: ${burgers.nameOfCategories}, Recipe: ${burgers.listOfRecipe.joinToString { it.name }}, " +
-            "состоящий из: ${burger.ingredients.joinToString { it.name }}")
-
-}
-
-class RecipeCategories(
+class RecipeCategory(
     val id: Int,
+    val imageURL: String,
     val nameOfCategories: String,
-    val listOfRecipe: Set<Recipe>,
-) {}
+    val listOfRecipe: List<Recipe>,
+)
 
 class Recipe(
     val id: Int,
+    val imageURL: String,
     val name: String,
-    val ingredients: Set<Ingredients>,
+    val ingredients: List<Ingredient>,
+    val cookingMethod: List<String>,
     var isFavorite: Boolean = false,
-) {}
+)
 
-class Ingredients(
+class Ingredient(
     val id: Int,
     val name: String,
-) {}
+)
