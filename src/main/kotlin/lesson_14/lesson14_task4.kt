80aa66c8f7735ef38646787be445d78df29202bd
= <=>
2024-03-12 18:24:51 +0500

    KS-14-4 solution added

---
+++ src/main/kotlin/lesson_14/lesson14_task4.kt
@@ -0,0 +1,26 @@
+package org.example.lesson_1.lesson_14
+
+fun main() {
+
+    val fobos = Satellite("Фобос")
+    val daymos = Satellite("Деймос")
+    val mars = Planet("Марс", listOf(fobos, daymos))
+
+    println("Впереди планета ${mars.name}! У неё ${mars.satellites.size} спутника: ${mars.satellites[0].name} и ${mars.satellites[1].name}.")
+
+}
+
+open class HeavenlyBody(
+    val name: String,
+    val isPresenceOfAtmosphere: Boolean = false,
+    val isSuitableForLanding: Boolean = false,
+)
+
+class Planet(
+    name: String,
+    val satellites: List<Satellite>,
+) : HeavenlyBody(name, isSuitableForLanding = true)
+
+class Satellite(
+    name: String,
+) : HeavenlyBody(name)
