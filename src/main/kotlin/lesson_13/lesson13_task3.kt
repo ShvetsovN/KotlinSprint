db176dccb190eeb8ec2d12266b1ea4834d474e1d
= <=>
2024-03-13 16:43:57 +0500

    KS-13-3 solution updated added company with "null" name

--- src/main/kotlin/lesson_13/lesson13_task3.kt
+++ src/main/kotlin/lesson_13/lesson13_task3.kt
@@ -6,7 +6,7 @@ fun main() {
     val contacts32 = Contacts3("Батон", 23456789, "Бетта")
     val contacts33 = Contacts3("Ватон", 3456789)
     val contacts34 = Contacts3("Гатон", 456789)
-    val contacts35 = Contacts3("Датон", 56789, null)
+    val contacts35 = Contacts3("Датон", 56789, "null")
     val contactsList = listOf(contacts31, contacts32, contacts33, contacts34, contacts35)
     contactsList.forEach { it.printInformation() }
 
