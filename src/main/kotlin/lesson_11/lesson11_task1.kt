d31d7089337913991d1119453deb4c6d31fc3c30
= <=>
2024-03-14 16:37:14 +0500

    KS-11-1 renamed User to User111

--- src/main/kotlin/lesson_11/lesson11_task1.kt
+++ src/main/kotlin/lesson_11/lesson11_task1.kt
@@ -2,13 +2,13 @@ package org.example.lesson_1.lesson_11
 
 fun main() {
 
-    val user1 = User(
+    val user1 = User111(
         id = 1,
         login = "Петр",
         password = "1234",
         email = "petr@redkaja.da",
     )
-    val user2 = User(
+    val user2 = User111(
         id = 2,
         login = "Павел",
         password = "4321",
@@ -20,7 +20,7 @@ fun main() {
 
 }
 
-class User(
+class User111(
     val id: Int,
     val login: String,
     val password: String,
