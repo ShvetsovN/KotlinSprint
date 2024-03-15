5bc6b672bc5fc4e3640d83aa07bac12233a9ce07
= <=>
2024-03-15 14:33:03 +0500

    KS-14-3 solution updated put colors into constant

--- src/main/kotlin/lesson_14/lesson14_task3.kt
+++ src/main/kotlin/lesson_14/lesson14_task3.kt
@@ -1,17 +1,20 @@
 package org.example.lesson_1.lesson_14
 
+const val WHITE_COLOR = "Белый"
+const val BLACK_COLOR = "Черный"
+
 fun main() {
 
-    val circle1431 = Circle("Белый", 6.0)
-    val circle1432 = Circle("Черный", 3.0)
+    val circle1431 = Circle(WHITE_COLOR, 6.0)
+    val circle1432 = Circle(BLACK_COLOR, 3.0)
 
-    val rectangle1431 = Rectangle("Черный", 2.0, 4.0)
-    val rectangle1432 = Rectangle("Белый", 3.0, 2.0)
+    val rectangle1431 = Rectangle(BLACK_COLOR, 2.0, 4.0)
+    val rectangle1432 = Rectangle(WHITE_COLOR, 3.0, 2.0)
 
     val listOfFigure = listOf(circle1431, circle1432, rectangle1431, rectangle1432)
 
-    val blackPerimeterSum = listOfFigure.filter { it.color == "Черный" }.sumOf { it.calculatePerimeter() }
-    val whiteAreaSum = listOfFigure.filter { it.color == "Белый" }.sumOf { it.calculatePerimeter() }
+    val blackPerimeterSum = listOfFigure.filter { it.color == BLACK_COLOR }.sumOf { it.calculatePerimeter() }
+    val whiteAreaSum = listOfFigure.filter { it.color == WHITE_COLOR }.sumOf { it.calculatePerimeter() }
 
     println("Сумма периметров всех черных фигур: $blackPerimeterSum")
     println("Сумма площадей всех белых фигур: $whiteAreaSum")
