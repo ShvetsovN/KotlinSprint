bf2d3161d5a3465a13d56639ef9d68b58eb50d5b
= <=>
2024-03-21 15:03:46 +0500

    KS-17-3 solution updated the getter and setter of all variables have been changed

--- src/main/kotlin/lesson_17/lesson17_task3.kt
+++ src/main/kotlin/lesson_17/lesson17_task3.kt
@@ -3,36 +3,31 @@ package org.example.lesson_1.lesson_17
 fun main() {
     val folder1731 = Folder173("Первая", 15, false)
 
-    folder1731.getName()
-    folder1731.setIsSecret(true)
-    folder1731.getName()
+    folder1731.name
+    folder1731.isSecret = true
+    folder1731.name
 
 }
 
 class Folder173(
-    private var name: String,
-    private var numberOfFolders: Int,
-    private var isSecret: Boolean,
+    _name: String,
+    _numberOfFolders: Int,
+    _isSecret: Boolean,
 ) {
-    fun getName(): String {
-        if (isSecret == false) {
-            return println("Имя папки: ${name}, количество файлов - ${getNumberOfFolders()}").toString()
-        } else {
-            return println("Скрытая папка, количество файлов - 0").toString()
+
+    val name: String = _name
+        get() = if (isSecret == false) println("Имя папки: ${field}, количество файлов - $numberOfFolders").toString()
+        else println("Скрытая папка, количество файлов - 0").toString()
+
+    var numberOfFolders: Int = _numberOfFolders
+        get() = field
+        set(value) {
+            field = value
         }
-    }
 
-    fun setName(newName: String) {
-        name = newName
-    }
-
-    fun getNumberOfFolders(): Int = numberOfFolders
-    fun setNumberOfFolders(newNumber: Int) {
-        numberOfFolders = newNumber
-    }
-
-    fun getIsSecret(): Boolean = isSecret
-    fun setIsSecret(newFlag: Boolean) {
-        isSecret = newFlag
-    }
+    var isSecret = _isSecret
+        get() = field
+        set(value) {
+            field = value
+        }
 }
