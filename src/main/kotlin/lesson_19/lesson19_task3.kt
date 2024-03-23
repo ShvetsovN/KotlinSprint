0514f261fcfc4883f42d20351c2ab76cb9e56309
= <=>
2024-03-23 16:54:42 +0500

    KS-19-3 solution added

---
+++ src/main/kotlin/lesson_19/lesson19_task3.kt
@@ -0,0 +1,22 @@
+package org.example.lesson_1.lesson_19
+
+fun main(){
+    val ship193 = SpaceShip193()
+    ship193.shipTakeOff()
+    ship193.shipShoot()
+
+}
+
+class SpaceShip193{
+    fun shipTakeOff(){
+        TODO("call shipLands()")
+    }
+
+    fun shipLands(){
+        TODO()
+    }
+
+    //TODO реализовать shipShoot()
+    fun shipShoot(){
+    }
+}
