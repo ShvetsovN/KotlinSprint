3efb9f2f23727f5d5438b599ef27a4006a461be4
= <=>
2024-03-25 18:15:57 +0500

    KS-19-4 solution updated rename and changed shoot fun to makeAShot() fun rename weapon fun to equipArms() fun

--- src/main/kotlin/lesson_19/lesson19_task4.kt
+++ src/main/kotlin/lesson_19/lesson19_task4.kt
@@ -3,54 +3,43 @@ package org.example.lesson_1.lesson_19
 fun main() {
 
     val tank194 = Tank194()
-    tank194.Shoot()
-    tank194.Weapon(Patron.BLUE)
-    tank194.Shoot()
-    tank194.Weapon(Patron.RED)
-    tank194.Shoot()
+    tank194.makeAShot()
+    tank194.equipArms(Patron.BLUE)
+    tank194.makeAShot()
+    tank194.equipArms(Patron.RED)
+    tank194.makeAShot()
 
 }
 
-enum class Patron(val dmg: Int) {
-    EMPTY(0) {
-        override fun getPatronType(): String {
-            return "Патроны отсутсвуют"
-        }
+enum class Patron(private val dmg: Int) {
+    EMPTY(0),
+    BLUE(5),
+    GREEN(10),
+    RED(20);
 
-    },
-    BLUE(5) {
-        override fun getPatronType(): String {
-            return "Синие патроны наносят $dmg единиц урона"
+    fun getPatronType(): String {
+        return when (this) {
+            BLUE -> "Синие патроны наносят $dmg единиц урона."
+            GREEN -> "Зеленые патроны наносят $dmg единиц урона."
+            RED -> "Красные патроны наносят $dmg единиц урона."
+            else -> "Патроны отсутствуют."
         }
-    },
-    GREEN(10) {
-        override fun getPatronType(): String {
-            return "Зеленые патроны наносят $dmg единиц урона"
-        }
-    },
-    RED(20) {
-        override fun getPatronType(): String {
-            return "Красные патроны наносят $dmg единиц урона"
-        }
-    };
-
-    abstract fun getPatronType(): String
+    }
 }
 
 class Tank194 {
-    private var currentPatron: Patron = Patron.EMPTY
-
-    fun Weapon(patron: Patron) {
-        currentPatron = patron
-        println("Танк заряжен ${currentPatron.name} патронами.")
+    private var patron: Patron = Patron.EMPTY
+    fun equipArms(currentPatron: Patron) {
+        patron = currentPatron
+        println("Танк заряжен ${currentPatron.name} патронами.\n")
     }
 
-    fun Shoot() {
-        when (currentPatron) {
-            Patron.BLUE -> println("Танк стреляет. ${Patron.BLUE.getPatronType()}")
-            Patron.GREEN -> println("Танк стреляет. ${Patron.GREEN.getPatronType()}")
-            Patron.RED -> println("Танк стреляет. ${Patron.RED.getPatronType()}")
-            else -> println(Patron.EMPTY.getPatronType())
+    fun makeAShot() {
+        if (patron == Patron.EMPTY) println("Танк не может стрелять. ${patron.getPatronType()}\n")
+        else {
+            println("Танк стреляет. ${patron.getPatronType()}")
+            patron = Patron.EMPTY
+            println(patron.getPatronType())
         }
     }
 }
