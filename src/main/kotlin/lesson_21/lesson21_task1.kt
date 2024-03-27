8b8c95b95aa4b64771a8b357e79436a359f9993a
= <=>
2024-03-27 15:10:21 +0500

    KS-21-1 solution updated changed vowelCount fun

--- src/main/kotlin/lesson_21/lesson21_task1.kt
+++ src/main/kotlin/lesson_21/lesson21_task1.kt
@@ -3,14 +3,15 @@ package org.example.lesson_1.lesson_21
 fun main() {
 
     val hello = "Hello"
-    val vowelCount = hello.vowelCount()
+    val setOfElement = setOf('q','e','y','u','i','o','a')
+    val vowelCount = hello.vowelCount{it in setOfElement}
 
     println(vowelCount)
 
 }
 
-fun String.vowelCount(): Int {
-    val currentNumberOfVowels = this.filter { it.lowercase() in "qeyuioa" }
-    return currentNumberOfVowels.length
+fun String.vowelCount(predicate: (Char) -> Boolean): Int {
+    var count = 0
+    for(element in this) if (predicate(element)) ++ count
+    return count
 }
-
